8a67008c37e4aecddcf7f80ba793288bebbdd74d
dev <dev@xyz.com>
2021-06-20 10:16:54 +0530

    local directory and codes

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Log;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+        for($i= 0; $i<10;$i++){
+            Log::info($faker->name());
+        }
+    }
+}
